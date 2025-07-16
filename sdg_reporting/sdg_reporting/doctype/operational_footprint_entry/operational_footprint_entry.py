7b6b846c5a462b0b1d1a4c74918a1a287beaf758
Emanuel Kagombora <emanuelkagombora28@gmail.com>
2025-07-16 23:27:33 +0300

    feat: add SDG Company Doctype with Operational Footprint child table

---
+++ sdg_reporting/sdg_reporting/doctype/operational_footprint_entry/operational_footprint_entry.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Emanuel Fidelis and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class OperationalFootprintEntry(Document):
+	pass
